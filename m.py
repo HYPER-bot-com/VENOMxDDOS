8011b73ce2f7b41bd214729f747e7e2f9dcd0a15
HYPER-bot-com <hypergamer6337@gmail.com>
2024-06-25 16:14:33 +0530

    Update m.py

--- m.py
+++ m.py
@@ -8,10 +8,10 @@ import datetime
 import os
 
 # insert your Telegram bot token here
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot('7329776309:AAFUlNpYqsx_An4Helqpc_Pz8UtnjO4Sn-4')
 
 # Admin user IDs
-admin_id = ["YOUR ID"]
+admin_id = ["6310751880"]
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
